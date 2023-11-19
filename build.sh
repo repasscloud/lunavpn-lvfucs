0b77be1b9a7b1e6b3600856a5c06013eccf9fe48
Danijel-James Wynyard <danijel@repasscloud.com>
2023-11-19 23:46:59 +1100

    update gh-action

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 # Build with dotnet
+dotnet publish -c Release -p:PublishSingleFile=true --self-contained true
 #dotnet publish -c Release -r osx-arm64 -p:PublishReadyToRun=true -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true
-dotnet publish -c Release -r linux-x64 -p:PublishReadyToRun=true -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true
+#dotnet publish -c Release -r linux-x64 -p:PublishReadyToRun=true -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true
