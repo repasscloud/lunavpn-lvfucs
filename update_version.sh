a2e18cc92558786bd6f9801b3de29581c98aba54
Danijel-James Wynyard <danijel@repasscloud.com>
2023-11-20 21:09:56 +1100

    update versioning script

--- update_version.sh
+++ update_version.sh
@@ -7,5 +7,5 @@ version_info=$(cat VERSION_INFO)
 escaped_version_info=$(printf '%s\n' "$version_info" | sed -e 's/[]\/$*.^[]/\\&/g')
 
 # Replace "vX.X.X" with the content of VERSION_INFO in all files
-find . -type f -exec sed -i "s/vX\.X\.X/${escaped_version_info}/g" {} +
+find . -type f -exec sed -i "s/vX\.X\.X/v${escaped_version_info}/g" {} +
 
