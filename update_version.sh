d6241eba6ed63ffc024641c64e87f01626a31778
Danijel-James Wynyard <danijel@repasscloud.com>
2023-11-20 21:06:14 +1100

    update version script for gh-actions

---
+++ update_version.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Read the content of VERSION_INFO
+version_info=$(cat VERSION_INFO)
+
+# Escape special characters in the version_info
+escaped_version_info=$(printf '%s\n' "$version_info" | sed -e 's/[]\/$*.^[]/\\&/g')
+
+# Replace "vX.X.X" with the content of VERSION_INFO in all files
+find . -type f -exec sed -i "s/vX\.X\.X/${escaped_version_info}/g" {} +
+
