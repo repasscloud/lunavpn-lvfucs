b4cb8aaafe27f4ff5a94161d9712adbdb6b8dfa0
Danijel-James Wynyard <danijel@repasscloud.com>
2023-12-10 03:24:37 +1100

    v2.0.1

--- Helper/Menu.cs
+++ Helper/Menu.cs
@@ -2,7 +2,7 @@
 {
     public class Menu
     {
-        private static string version = "v2.0.0";
+        private static string version = "v2.0.1";
 
         private static void PrintHeader()
         {
